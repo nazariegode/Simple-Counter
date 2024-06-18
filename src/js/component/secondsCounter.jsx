32b53fed0daff5fb3d937855a826a317f17b01df
David Nazariego Ulacio <dnazariego@gmail.com>
2024-06-18 20:10:05 +0000

    counter

---
+++ src/js/component/secondsCounter.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const SecondsCounter = () => {
+	return (
+		<div className="container">
+			<div className="progress">
+			</div>
+			<div className="counter-icon">
+				hola
+			</div>
+			<div className="counter-one">
+				1
+			</div>
+			<div className="counter-two">
+				2
+			</div>
+			<div className="counter-three">
+				3
+			</div>
+		</div>
+	);
+};
+
+export default SecondsCounter;
